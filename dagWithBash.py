3e544c5dfac8ca3845aa0d2d971c71c5ba53a483
kaan4dev <kaan4dev@gmail.com>
2025-08-11 21:41:01 +0300

    m

---
+++ dagWithBash.py
@@ -0,0 +1,33 @@
+from datetime import timedelta
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'your_name_here',
+    'start_date': days_ago(0),
+    'email': ['your_email_here'],
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'my-first-dag',
+    default_args=default_args,
+    description='My first DAG',
+    schedule_interval=timedelta(days=1),
+)
+
+extract = BashOperator(
+    task_id='extract',
+    bash_command='cut -d":" -f1,3,6 /etc/passwd > /home/project/airflow/dags/extracted-data.txt',
+    dag=dag,
+)
+
+transform_and_load = BashOperator(
+    task_id='transform',
+    bash_command='tr ":" "," < /home/project/airflow/dags/extracted-data.txt > /home/project/airflow/dags/transformed-data.csv',
+    dag=dag,
+)
+
+extract >> transform_and_load
